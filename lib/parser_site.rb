759e7a8e62a6ee828a120d0846f80e29effa8890
quietpochatok <bosroin@gmail.com>
2021-01-18 21:44:45 +0500

    Refactor: delete unless variable in #parsing_from_site

--- lib/parser_site.rb
+++ lib/parser_site.rb
@@ -8,9 +8,7 @@ module ParserSite
   extend self
 
   def parsing_from_site
-    url = 'https://ru.wikipedia.org/wiki/250'
-    url << CGI.escape('_лучших_фильмов_по_версии_IMDb')
-    doc = Nokogiri::HTML(URI.open(url))
+    doc = Nokogiri::HTML(URI.open('https://ru.wikipedia.org/wiki/250' << CGI.escape('_лучших_фильмов_по_версии_IMDb')))
 
     table_row = doc.css('#mw-content-text .mw-parser-output table/tbody/tr')
       data_films = table_row.map { |row| row.css('td/a').map(&:content) }
