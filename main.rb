ddf9af6c0905caffee6ab596870443618dc44406
quietpochatok <bosroin@gmail.com>
2021-01-19 21:07:03 +0500

    Delete module(ConsoleInterface) and logic module move to main.rb

--- main.rb
+++ main.rb
@@ -1,22 +1,23 @@
 require_relative 'lib/film'
 require_relative 'lib/film_collection'
-require_relative 'lib/console_interface'
 
 filenames = "#{__dir__}/data/"
 
-puts ConsoleInterface.greeting_user
-user_choice_sourse_for_recom_films = ConsoleInterface.choice_sourse_for_recomendation
+puts  "Программа «Фильм на вечер»\n\nВыберите вариант источника списка фильмов
+        1. Из локальных данных
+        2. Со страницы википедии, в рубрике 150 лучших фильмов"
+
+user_choice_sourse_for_recom_films = STDIN.gets.to_i
 
 films =
   user_choice_sourse_for_recom_films == 1 ? FilmCollection.from_dir(filenames) : FilmCollection.from_site
 
 films.directors.reject { |i| i == nil }.each_with_index { |director, index| puts "#{index + 1}: #{director}" }
 
-user_choice_director = ConsoleInterface.input_choice
+user_choice_director = STDIN.gets.to_i
 
 director = films.directors[user_choice_director]
 
 films_for_user = films.films_by_director(director).sample
 
-output_message = ConsoleInterface.message_with_film(films_for_user)
-puts output_message
+puts "И сегодня вечером рекомендую посмотреть:\n#{films_for_user.to_s}"
