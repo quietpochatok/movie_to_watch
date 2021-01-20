584e05ea1f0760740bae24430acead937a384ae3
quietpochatok <bosroin@gmail.com>
2021-01-20 22:19:26 +0500

    Refactor: change code by code style in film_collection.rb;parser_site.rb

--- lib/parser_site.rb
+++ lib/parser_site.rb
@@ -1,14 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
-require "net/http"
-require "rexml/document"
 require "cgi"
 
 module ParserSite
+  YOUR_URL = 'https://ru.wikipedia.org/wiki/250' << CGI.escape('_лучших_фильмов_по_версии_IMDb')
   extend self
 
-  def parsing_from_site
-    doc = Nokogiri::HTML(URI.open('https://ru.wikipedia.org/wiki/250' << CGI.escape('_лучших_фильмов_по_версии_IMDb')))
+  def parsing_page_site
+    doc = Nokogiri::HTML(URI.open(YOUR_URL))
 
     table_row = doc.css('#mw-content-text .mw-parser-output table/tbody/tr')
       data_films = table_row.map { |row| row.css('td/a').map(&:content) }
